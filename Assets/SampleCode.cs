da486f248dd18ca6ab116f5d90fa70b51771cae1
Goo <kuwoods@hotmail.com>
2020-10-01 20:39:18 +0800

    First import.

---
+++ Assets/SampleCode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using MLTool;
+
+public class SampleCode : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset lines = null;
+    [SerializeField]
+    private Dropdown dropDown = null;
+    [SerializeField]
+    private Text sampleText = null;
+
+    private void Start()
+    {
+        var languageKey = Application.systemLanguage == SystemLanguage.ChineseTraditional ? "zh-tw" : "en";
+        WordDic.Init(languageKey, lines.text);
+        sampleText.MutliText("test1");
+    }
+
+    public void OnDropDownChange(int value)
+    {
+        var languageKey = value == 0 ? "zh-tw" : "en";
+        WordDic.Init(languageKey, lines.text);
+        sampleText.MutliText("test1");
+    }
+}
